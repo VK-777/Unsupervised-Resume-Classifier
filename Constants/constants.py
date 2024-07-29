b873a9da64f295afb04fe22344aa568ba99c0f08
V <130358370+VK-777@users.noreply.github.com>
2024-07-29 15:17:07 +0530

    Update constants.py

--- Constants/constants.py
+++ Constants/constants.py
@@ -1,6 +1,6 @@
-LOG_FILE_PATH = "C:/#KIIT/RC2.1/Logging"
-DATABASE_URL = "postgresql://postgres:1410@localhost:5432/postgres"
-JOB_SKILLS_FILEPATH = "C:/#KIIT/RC2.1/job_skills.json"
-JOB_TITLES_FILEPATH = "C:/#KIIT/RC2.1/job_titles.json"
-API_KEYS_FILEPATH = "C:/#KIIT/RC2.1/keys.txt"
+LOG_FILE_PATH = "Logging/file/path"
+DATABASE_URL = "postgresql_database_link_here"
+JOB_SKILLS_FILEPATH = "job_skills.json/file/path"
+JOB_TITLES_FILEPATH = "job_titles.json/file/path"
+API_KEYS_FILEPATH = "keys.txt/file/path"
 CHUNK_SIZE = 100
